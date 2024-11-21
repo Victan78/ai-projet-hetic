94e8ef1a99a29a1fc12c597930ad049bc30ac04d
Admin <admin@MacBook-Pro-de-Admin.local>
2024-11-21 16:17:59 +0100

    Initial commit

---
+++ test.py
@@ -0,0 +1,14 @@
+import ollama 
+
+
+question = "What were the iPhone's best new features"
+stream = ollama.chat(
+  model="llama3.2", stream=True,
+  messages = [
+    { "role": "user", 'content': f"Question: {question}"}
+  ]
+)
+for chunk in stream:
+  print(chunk['message']['content'], end='', flush=True)
+
+
